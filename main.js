8098bee8f2d280d9effd945b69b5344416a24d56
Denis Righi <denis.righi-ext@ab-inbev.com>
2022-07-25 16:35:58 -0300

    textos dinâmicos

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-alert('Olá mundo');
+function tocaSom (idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
+    
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    listaDeTeclas[contador].onClick = function () {
+        tocaSom('#som_tecla_pom');
+    }
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
