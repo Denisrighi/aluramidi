dc604fc655427233a4c13ceaee94625d7b8cb54b
Denis Righi <denis.righi-ext@ab-inbev.com>
2022-07-26 16:30:36 -0300

    aluramidi final

--- main.js
+++ main.js
@@ -1,28 +1,42 @@
-function tocaSom (idElementoAudio) {
+function tocaSom (seletorAudio) {
 
-    document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(seletorAudio);
     
-}
+
+    if (elemento && elemento.localName === 'audio') {
+            elemento.play();
+        }
+
+        else {
+         //alert('ELemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+        }
+    } 
+    
+
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-//enquanto
-while (contador < listaDeTeclas.length) {
+//enquanto/para
+for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;// template string
 
-    // template string
-    const idAudio = `#som_${instrumento}`;
-    console.log(idAudio);
-
-    listaDeTeclas[contador].onClick = function () {
+    tecla.onClick = function () {
         tocaSom(idAudio);
     }
 
-    contador = contador + 1;
+    tecla.onkeydown = function (evento) {
 
-    console.log(contador);
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
